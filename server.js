c8706aa6ec275ab24a85239bba94c4fd5c5a5eb3
Jihee Seo <jseo33@myseneca.ca>
2022-09-16 23:55:40 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Jihee Seo Student ID: 161875190 Date: Sep 16 2022
+*  Cyclic Link: _______________________________________________________________
+*
+********************************************************************************/ 
+
 const express = require('express');
 const cors = require('cors');
 const MoviesDB = require("./modules/moviesDB.js");
